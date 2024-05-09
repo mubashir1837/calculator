ca075b31b0a0c09c790cbbccbbbac03a735d29b6
My Name <someone@email.com>
2024-05-10 01:02:47 +0500

    Calculator

---
+++ script.js
@@ -0,0 +1,22 @@
+const display = document.getElementById("display")
+
+
+
+function appendToDisplay(input){
+    display.value += input;
+
+}
+
+function clearDisplay(){
+    display.value = "";
+
+}
+
+function calculate(){
+    try{
+        display.value = eval(display.value);
+    }
+    catch(error){
+        display.value = "Error";
+    }
+}
